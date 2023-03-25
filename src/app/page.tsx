31855fe549a2b28ed77ba07085d0fca258b405d3
JawadAmmar700 <JawadAmmarCode700@gmail.com>
2023-03-25 17:34:45 +0200

    trying for /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { headers } from "next/headers";
 import { getSession } from "@/lib/auth-session";
 import SignoutBtn from "@/components/signout-btn";
 
+export const dynamic = "force-static";
+
 export default async function Home() {
   const session = await getSession(headers().get("cookie") ?? "");
 
