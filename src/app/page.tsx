e69252e9185fa4a48d64f9408c4878759e4946d1
JawadAmmar700 <JawadAmmarCode700@gmail.com>
2023-03-25 17:38:35 +0200

    remove static /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import { headers } from "next/headers";
 import { getSession } from "@/lib/auth-session";
 import SignoutBtn from "@/components/signout-btn";
 
-export const dynamic = "force-static";
-
 export default async function Home() {
   const session = await getSession(headers().get("cookie") ?? "");
 
