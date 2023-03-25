9c7349441dc8dbd78802e057d57845f9f1fc5abb
JawadAmmar700 <JawadAmmarCode700@gmail.com>
2023-03-25 17:30:56 +0200

    /auth-force-static

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const session = await getSession(headers().get("cookie") ?? "");
 
   return (
-    <main className="">
+    <main className=""></main>
       <h1>Senior</h1>
       <pre>{JSON.stringify(session, null, 2)}</pre>
       <SignoutBtn />
