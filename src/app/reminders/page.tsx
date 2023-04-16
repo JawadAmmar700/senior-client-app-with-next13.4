4c98db229ddd4bd4d5dc1e9cb5b75ac705526608
JawadAmmar700 <JawadAmmarCode700@gmail.com>
2023-04-16 21:17:31 +0300

    test7

--- src/app/reminders/page.tsx
+++ src/app/reminders/page.tsx
@@ -1,15 +1,18 @@
-// import CalendarTodo from "@/components/remender/calendar";
-// import ReminderTodos from "@/components/remender/reminderTodos";
+import CalendarTodo from "@/components/remender/calendar";
+import ReminderTodos from "@/components/remender/reminderTodos";
+import { Suspense } from "react";
+
+export const revalidate = 30;
 
 const Page = () => {
   return (
     <div>
       <h1>Reminders</h1>
       <br />
-      {/* <Suspense fallback={<div>Loading...</div>}> */}
-
-      {/* <ReminderTodos /> */}
-      {/* </Suspense> */}
+      <Suspense fallback={<div>Loading...</div>}>
+        {/* @ts-expect-error Server Component */}
+        <ReminderTodos />
+      </Suspense>
       <br />
       <br />
       <br />
